20a223209f3259374711fb6200aaf6cd547f4e5d
adamteddychang <adamchang0725@gmail.com>
2022-05-06 17:52:14 +0200

    merge two sorted list

---
+++ 4_merge_two_sorted_list.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+ var mergeTwoLists = function(list1, list2) {
+    
+  let sorted = new ListNode();
+  let head = sorted ;
+  while (list1 && list2){
+      if(list1.val < list2.val){
+          sorted.next = list1;
+          list1 = list1.next;
+      }else {
+          sorted.next = list2;
+          list2 = list2.next;
+      }
+      sorted = sorted.next;
+  }
+  if (list1) sorted.next = list1;
+  if (list2) sorted.next = list2;
+  return head.next
+};
--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 1. [Two_sum](https://leetcode.com/problems/two-sum/)
 2. [Validate subsequence](https://www.algoexpert.io/questions/Validate%20Subsequence)
 3. [Is Subsequence](https://leetcode.com/problems/is-subsequence/)
+4. [Merge two sorted lists](https://leetcode.com/problems/merge-two-sorted-lists/)
