bf3e0aee54ea176edef3c2f096401326953300ec
adamteddychang <adamchang0725@gmail.com>
2022-05-19 22:14:47 +0200

    loop sqrt

--- challenges/30_sqrt.js
+++ challenges/30_sqrt.js
@@ -5,3 +5,11 @@
 const mySqrt = function (x) {
   return Math.floor(Math.sqrt(x));
 };
+
+const mySqrt2 = function (x) {
+  let i = 0;
+  while (i * i < x) i++;
+
+  if (i * i !== x) i--;
+  return i;
+};
